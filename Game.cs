c299ca0bba73f31b957ffff42c9f969fe0c60efb
Matthew-Gwin <Matthew.Gwin@hologic.com>
2022-06-02 17:11:22 -0400

    Add game class and player control

---
+++ Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game
+{
+    internal class Game
+    {
+        public IDictionary<int,Player> players;
+
+        public Game()
+        {
+        }
+
+        public void AddPlayer(Player p)
+        {
+            
+            int UID = this.generateID()
+            if players.ContainsKey(UID){
+                while (if players.ContainsKey(UID))
+                {
+                        UID = this.generateID();
+                }
+            }
+            players.Add(UID, p);
+        }
+        private int generateID()
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, 99999);
+        }
+    }
+}
--- Player.cs
+++ Player.cs
@@ -7,14 +7,13 @@ namespace Card_Game
     class Player
     {
         private string name { get; }
-        private int UID { get; }
+        private int UID { get; set; }
         private bool isPlaying { get; set; }
         private Hand hand { get; set; }
 
         public Player(string playerName)
         {
-            Random rnd = new Random();
-            this.UID = rnd.Next(0, 99999);
+
             this.isPlaying = true;
             this.hand = new Hand();
             this.name = playerName;
