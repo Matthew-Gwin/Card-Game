15f65ea7ce3442972e6bbf9a4106a2ec02d638c9
Matthew-Gwin <Matthew.Gwin@hologic.com>
2022-06-06 11:41:24 -0400

    Create BlackJack Structure

---
+++ BlackJack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game
+{
+    internal class BlackJack : Game
+    {
+
+        public BlackJack() : base()
+        {
+            base.players.Add()
+            BlackJackPlayer Dealer = new BlackJackPlayer("Dealer");
+        } 
+    }
+}
---
+++ BlackJackPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Game
+{
+    internal class BlackJackPlayer : Player
+    {
+        public BlackJackPlayer(string playerName) : base(playerName)
+        {
+        }
+    }
+}
